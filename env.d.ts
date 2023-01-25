1ec656990deb1a393d6e3ae9c4cb8ddb1ade514c
ej shafran <116496520+ej-shafran@users.noreply.github.com>
2023-01-25 02:40:18 +0200

    Add other providers (#5)
    * Add google client
    
    * Add github client
    
    * Add authentication for store

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,14 @@
 namespace NodeJS {
   interface ProcessEnv {
     ROLE: "ADMIN" | "CUSTOMER";
+
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+
+    GITHUB_CLIENT_ID: string;
+    GITHUB_CLIENT_SECRET: string;
+
+    NEXTAUTH_URL: string;
+    NEXTAUTH_SECRET: string;
   }
 }
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+import GithubProvider from 'next-auth/providers/github';
 import { compare } from 'bcrypt';
 
 import db from 'db';
@@ -21,6 +23,14 @@ export default NextAuth({
 
         return user;
       },
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     })
   ],
   pages: {
@@ -28,13 +38,13 @@ export default NextAuth({
   },
   callbacks: {
     jwt(params) {
-      if(params.user && params.user.role) {
+      if (params.user && params.user.role) {
         params.token.role = params.user.role;
       }
       return params.token;
     },
     session(params) {
-      if(params.token.role) {
+      if (params.token.role) {
         params.session.user.role = params.token.role;
       }
       return params.session;
